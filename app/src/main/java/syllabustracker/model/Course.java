79be74b35b6b75369f82406996dbabf53f4f803f
Oğulcan Gökay <104510078+OgulcanGokay@users.noreply.github.com>
2023-12-21 18:34:56 +0300

    Update Course.java
    Added syllabusID method

--- app/src/main/java/syllabustracker/model/Course.java
+++ app/src/main/java/syllabustracker/model/Course.java
@@ -6,19 +6,23 @@ public class Course {
 
    private String courseID;
    private String courseName;
-   private ArrayList<Syllabus> syllabi = new ArrayList<> ();
-   private ArrayList<ChangeLog> changeHistory = new ArrayList<>();
+   private int versionNo;
+   private ArrayList<Syllabus> syllabi;
+   private ArrayList<ChangeLog> changeHistory;
 
-   
+
    public Course(String courseID, String courseName, ArrayList<Syllabus> syllabi, ArrayList<ChangeLog> changeHistory) {
+      syllabi = new ArrayList<>();
+      changeHistory = new ArrayList<>();
       this.courseID = courseID;
       this.courseName = courseName;
       this.syllabi = syllabi;
       this.changeHistory = changeHistory;
    }
 
+   private String syllabusID(int versionNo){
+      return courseID + "V" + versionNo;
 
-   
-
+   }
      
 }
