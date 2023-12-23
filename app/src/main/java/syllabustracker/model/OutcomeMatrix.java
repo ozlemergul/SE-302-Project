99677621ad2899b3b1f32b22e2daef6742ce5210
MertCtn <mertctnstd35@gmail.com>
2023-12-23 17:20:11 +0300

    Added outcome matrix insert method.

--- app/src/main/java/syllabustracker/model/OutcomeMatrix.java
+++ app/src/main/java/syllabustracker/model/OutcomeMatrix.java
@@ -3,14 +3,47 @@ package syllabustracker.model;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import syllabustracker.util.Database;
+
 public class OutcomeMatrix {
 
     private ArrayList<String> programOutcomes = new ArrayList<>();
-    private HashMap<Integer,String> contributionLevel = new HashMap<>();  // String side is used for showing which LO's are related with this outcome in a form of 001001 means 3,6
+    private HashMap<Integer,String> contributionLevelMatrix = new HashMap<>();  // String side is used for showing which LO's are related with this outcome in a form of 001001 means 3,6
 
-    public OutcomeMatrix(ArrayList<String> programOutcomes, HashMap<Integer, String> contributionLevel) {
+    public OutcomeMatrix(ArrayList<String> programOutcomes, HashMap<Integer, String> contributionLevelMatrix) {
         this.programOutcomes = programOutcomes;
-        this.contributionLevel = contributionLevel;
+        this.contributionLevelMatrix = contributionLevelMatrix;
+    }
+
+    public void insertOutcomeMatrix(Database db,String syllabusID){
+
+        if(db.getConnection() == null){
+            db.connect();
+        }
+
+        // Program outcomes database insert
+        for(int i=0; i<programOutcomes.size(); i++){
+            db.insertData("program_compentency",i+1,programOutcomes.get(i));
+        }
+
+        // Contribution levels and realted learning outcomes insert
+        ArrayList<Integer> contributionLevels = new ArrayList<>();
+        ArrayList<String> relatedLearningOutcomes = new ArrayList<>();
+
+        for(Integer contributionLevel : contributionLevelMatrix.keySet()){
+            contributionLevels.add(contributionLevel);
+        }
+
+        for(String relatedLOs : contributionLevelMatrix.values()){
+            relatedLearningOutcomes.add(relatedLOs);
+        }
+
+        for(int j=0;j<contributionLevels.size();j++){
+            db.insertData("course_compentency",j+1,syllabusID,contributionLevels.get(j),relatedLearningOutcomes.get(j));
+        }
+
+        db.close();
+
     }
 
     
